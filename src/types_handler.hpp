d1435d834f6d4fa061f6058f154ef84ee3a17b46
zrempz <rmontanezp@unsa.edu.pe>
2025-05-14 21:43:03 -0500

    Delete double function declaration

--- src/types_handler.hpp
+++ src/types_handler.hpp
@@ -4,9 +4,7 @@ enum DATATYPE { FLOAT, INT, STRING };
 
 void process_type(char &c, bool &has_sign, bool &has_digits, bool &has_dot,
                   bool &is_valid);
-void process_type(char &c, bool &has_sign, bool &has_digits, bool &has_dot,
-                  bool &valid);
-
+DATATYPE determine_type(bool has_digits, bool has_dot, bool is_valid);
 bool is_sign(const char c);
 bool is_dot(const char c);
 bool is_digit(const char c);
